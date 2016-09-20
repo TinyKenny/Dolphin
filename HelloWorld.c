65fcad32fa6cea0ac6c2ba43a8d38efd8dfcbad0
albatrossPasta <lukastonneman@hotmail.com>
2016-09-20 20:12:27 +0200

    lade till helloWorld för att testa funktion

---
+++ HelloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   printf("Hello World! says Lukas\n");
+   //ey jonte läggg till så att den skriver ut något mer så att vi ser att allt funkar
+   system("pause");
+   return 0;
+}
