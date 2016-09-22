b0da00c848ac3c1379f37ecbd89ecaae416be604
TinyKenny <Jonathan@rawet.se>
2016-09-22 10:18:05 +0200

    i did stuff
    are you proud?

--- HelloWorld.c
+++ HelloWorld.c
@@ -6,5 +6,8 @@ int main()
    printf("Hello World! says Lukas\n");
    //ey jonte läggg till så att den skriver ut något mer så att vi ser att allt funkar
    system("pause");
+   printf("I forgot to bring the usb that i have the compiler on, so you have to do the compiling for now");
+   //jag gissar att det inte händer en massa dålig skit av att jag sätter in en tom rad mitt i kåden, stämmer det?
+   system("pause");
    return 0;
 }
