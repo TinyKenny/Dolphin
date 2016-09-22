8f62599106d602ffa6821ebbc9a8da14b8e812f6
TinyKenny <Jonathan@rawet.se>
2016-09-22 10:52:24 +0200

    trivial
    minor syntax practice

--- HelloWorld.c
+++ HelloWorld.c
@@ -8,6 +8,7 @@ int main()
    system("pause");
    printf("I forgot to bring the usb that i have the compiler on, so you have to do the compiling for now");
    //jag gissar att det inte händer en massa dålig skit av att jag sätter in en tom rad mitt i kåden, stämmer det?
+   /*so many ways to comment! :O*/
    system("pause");
    return 0;
 }
