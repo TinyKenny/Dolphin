db08906a03f7f91a3d9d89d155582f0b9e27b36a
TinyKenny <Jonathan@rawet.se>
2016-09-22 13:35:12 +0200

    trivial
    practice

--- HelloWorld.c
+++ HelloWorld.c
@@ -6,7 +6,7 @@ int main()
    printf("Hello World! says Lukas\n");
    //ey jonte läggg till så att den skriver ut något mer så att vi ser att allt funkar
    system("pause");
-   printf("I forgot to bring the usb that i have the compiler on, so you have to do the compiling for now");
+   printf("I forgot to bring the usb that i have the compiler on, so you have to do the compiling for now\n");
    //jag gissar att det inte händer en massa dålig skit av att jag sätter in en tom rad mitt i kåden, stämmer det?
    /*so many ways to comment! :O*/
    system("pause");
