0391019a0aefe50c338ccb7518080c822594971f
albatrossPasta <lukastonneman@hotmail.com>
2016-10-19 13:25:11 +0200

    fixar SP_server

--- JontePactice/SP_server.py
+++ JontePactice/SP_server.py
@@ -14,18 +14,23 @@ except socket.error as e:
 s.listen(5)
 print("Waiting for connection")
 
-def threaded_client_handler(conn):
+def threaded_client_handler(conn, IPv4, port):
     conn.send(str.encode("Hello World!"))
-    print("yes this is the client speaking")
     while 1:
-        data = conn.recv(2048)
-        reply = 'Server output: ' + data.decode('utf-8')
-        if not data:
+
+        try:
+            data = conn.recv(2048)
+            if not data:
+                break
+            reply = 'client says: ' + data.decode('utf-8')
+            #conn.sendall(str.encode(reply))
+        except socket.error as e:
+            print(e)
             break
-        conn.sendall(str.encode(reply))
+    print("closed connection to"+IPv4+":"+port)
     conn.close()
 
 while 1:
     conn, addr = s.accept()
     print('connected to ' + addr[0] + ':' + str(addr[1]))
-    start_new_thread(threaded_client_handler, (conn, ))
+    start_new_thread(threaded_client_handler, (conn,  addr[0], str(addr[1])))
