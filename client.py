80f05d1775489f5b58a4abd01030b92c694fa209
albatrossPasta <lukastonneman@hotmail.com>
2016-10-24 17:39:18 +0200

    see description
    flyttade runt lite saker
    optimerat lite
    fixat lite med trådarna
    lagt till och delvis implementerat kommandon

--- client.py
+++ client.py
@@ -2,64 +2,113 @@ import socket
 import os
 import threading
 #Yes, I know, none of the comments are in swedish. deal with it
+s=socket
+chat_server="127.0.0.1"
+port=5555
+username=""
+developer_mode=0
+
+command_dict={"-help":"\t\t\tview this page",
+			  "-quit":"\t\t\texit program",
+			  "-save prof [name]":"save profile settings as [name]",
+			  "-del prof [name]":"delete profile [name]",
+			  "-view prof":"\t\tviews your saver profiles",
+			  "-edit prof [name]":"edit profile [name]"}
+
+root_command_dict={"terminate":"\t\tterminates the server",
+				   "-kick [username]":"kicks [username] from the server"}
+
+if (input("Load profile or connect to an unsaved server?y/n")).lower() == 'y':
+	print("not implemented yet")
+	# Add code to import a profile  here
+	os._exit(0)  # since no profile is selected, it is useless to try to connect
+else:
+	username = input("Enter your username:\n>>>")
+	if username == "root":  # enables client developer mode
+		developer_mode = True  # Boolean that keeps track of wether developer mode is active or not
+		print("You are now root user (admin)")
+		username = input("Username on server?\n")  # to make it possible to enable client dev mode without connecting as root
+
+while True:
+		address_protocol = input("IP protocol? (IPv4 or IPv6)\n>>>")
+		if address_protocol.lower() == "ipv4":
+			s=socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creates a socket object for IPv4
+			valid_protocol = True #IPv4 is a valid protocol
+			break #IPv4 is a valid protocol
+		elif address_protocol.lower() == "ipv6":
+			s=socket.socket(socket.AF_INET6, sokcet.SOCK_STREAM) #creates a socket object for IPv6
+			valid_protocol = True #IPv6 is a valid protocol
+			s=socket.socket(socket.AF_INET6, socket.SOCK_STREAM) #creates a socket object for IPv6
+			break #IPv6 is a valid protocol
+		else:
+			print("Invalid network protocol")
+
+#comment these away if you want to connect to 127.0.0.1:5555
+chat_server = input("IP address?\n>>>")
+port = int(input("Port?\n>>>")) #error will be raised if you enter a string, GUI will solve that
+
+try:
+	s.connect((chat_server,port))
+	print ("Connected to:",chat_server+":"+str(port))
+	data = s.recv(2048) #recieve the message of the day
+	print("Message of the day: " + str(data.decode('utf-8'))) #currently just a random quote
+	s.send(str.encode(username)) #inform the server of your username
+except socket.error as e: #couldn't connect to given IP + port
+	print("Cound not connect to",chat_server+":"+str(port) + "\n" + str(e))
+	os._exit(1)
+
 def send_messages(): #the function that sends messages
 	while True:
 		client_message = input(">>>")
-		if client_message == "quit": #ends the while-loop, go to line 20
-			break
+		if client_message[0:1] == "-": #lauches command intepreter
+			if client_message=="-quit":
+				break
+			elif client_message=="-help":
+				for command, desc in command_dict.items():
+					print(command +"\t" +desc)
+				if developer_mode:
+					print("ROOT COMMANDS:")
+					for command, desc in root_command_dict.items():
+						print(command + "\t" + desc)
+
+			elif client_message[0:5]=="-kick":
+				s.send(str.encode(client_message))
+
+			elif client_message[0:11] == "-save prof ":
+				print("command not implemented")
+
+			elif client_message[0:10] == "-del prof ":
+				print("command not implemented")
+
+			elif client_message[0:11] == "-edit prof ":
+				print("command not implemented")
+
+			elif client_message[0:10] == "-view prof":
+				print("command not implemented")
+
+			else:
+				print("Unknown command:"+client_message)
 		else:
 			s.send(str.encode(client_message))
 	s.close()
 	return
+
 def recieve_messages(): #the function that recieves messages
 	while True:
 		try:
 			message_data = s.recv(2048)
 			server_message = message_data.decode('utf-8')
 			print (server_message)
-		except ConnectionAbortedError as e: #this will happen when the user enters "quit" or when the server is shut down
-			print (e) #prints out the error that specifies how the connection was lost
+			if server_message=="root:-kick " + username:
+				raise ConnectionError
+		except ConnectionError as e: #this will happen when the user enters "quit" or when the server is shut down
 			print("Disconnected from:", chat_server)
-			os.system("pause")
-			os._exit(1)
 			break
 	return
-username = input("Enter your username:\n>>>")
-if username == "root": #enables client developer mode
-	developer_mode = True #Boolean that keeps track of wether developer mode is active or not
-	valid_protocol = False #you haven't even entered a protocol yet! :P
-	while valid_protocol == False:
-		address_protocol = input("IP protocol? (IPv4 or IPv6)\n")
-		if address_protocol.lower() == "ipv4":
-			s=socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creates a socket object for IPv4
-			valid_protocol = True #IPv4 is a valid protocol
-		elif address_protocol.lower() == "ipv6":
-			s=socket.socket(socket.AF_INET6, socket.SOCK_STREAM) #creates a socket object for IPv6
-			valid_protocol = True #IPv6 is a valid protocol
-		else:
-			print("Invalid network protocol")
-	chat_server = input("IP address?\n")
-	port = int(input("Port?\n"))
-	username = input("Username on server?\n") #to make it possible to enable client dev mode without connecting as root
-else: #default values
-	developer_mode = False #self-explanatory
-	chat_server="0:0:0:0:0:0:0:1" #server address
-	port=5555 #server port
-	s=socket.socket(socket.AF_INET6, socket.SOCK_STREAM) #creates socket object for IPv6
 
-try:
-	s.connect((chat_server,port))
-	print ("connected to:",chat_server)
-	data = s.recv(2048) #recieve the message of the day
-	motd = "Message of the day: " + data.decode('utf-8') #currently just a random quote
-	print(str(motd))
-	s.send(str.encode(username)) #inform the server of your username
-except socket.error as e: #couldn't connect to given IP + port
-	print(e)
-	os.system("pause")
-	os._exit(1)
-finally:
-	sender = threading.Thread(target=send_messages,daemon=0) #function @ line 5
-	reciever = threading.Thread(target=recieve_messages,daemon=0) #function @ line 14
-	sender.start()
-	reciever.start()
+sender = threading.Thread(target=send_messages,daemon=0)
+reciever = threading.Thread(target=recieve_messages,daemon=0)
+sender.start()
+reciever.start()
+sender.join()
+reciever.join()
