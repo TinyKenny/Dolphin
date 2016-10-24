caafdd2e80af252b5b4dc47e482f6ee528b5788f
albatrossPasta <lukastonneman@hotmail.com>
2016-10-24 18:00:18 +0200

    bugg fixning från förra commit

--- client.py
+++ client.py
@@ -1,61 +1,6 @@
 import socket
 import os
 import threading
-#Yes, I know, none of the comments are in swedish. deal with it
-s=socket
-chat_server="127.0.0.1"
-port=5555
-username=""
-developer_mode=0
-
-command_dict={"-help":"\t\t\tview this page",
-			  "-quit":"\t\t\texit program",
-			  "-save prof [name]":"save profile settings as [name]",
-			  "-del prof [name]":"delete profile [name]",
-			  "-view prof":"\t\tviews your saver profiles",
-			  "-edit prof [name]":"edit profile [name]"}
-
-root_command_dict={"terminate":"\t\tterminates the server",
-				   "-kick [username]":"kicks [username] from the server"}
-
-if (input("Load profile or connect to an unsaved server?y/n")).lower() == 'y':
-	print("not implemented yet")
-	# Add code to import a profile  here
-	os._exit(0)  # since no profile is selected, it is useless to try to connect
-else:
-	username = input("Enter your username:\n>>>")
-	if username == "root":  # enables client developer mode
-		developer_mode = True  # Boolean that keeps track of wether developer mode is active or not
-		print("You are now root user (admin)")
-		username = input("Username on server?\n")  # to make it possible to enable client dev mode without connecting as root
-
-while True:
-		address_protocol = input("IP protocol? (IPv4 or IPv6)\n>>>")
-		if address_protocol.lower() == "ipv4":
-			s=socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creates a socket object for IPv4
-			valid_protocol = True #IPv4 is a valid protocol
-			break #IPv4 is a valid protocol
-		elif address_protocol.lower() == "ipv6":
-			s=socket.socket(socket.AF_INET6, sokcet.SOCK_STREAM) #creates a socket object for IPv6
-			valid_protocol = True #IPv6 is a valid protocol
-			s=socket.socket(socket.AF_INET6, socket.SOCK_STREAM) #creates a socket object for IPv6
-			break #IPv6 is a valid protocol
-		else:
-			print("Invalid network protocol")
-
-#comment these away if you want to connect to 127.0.0.1:5555
-chat_server = input("IP address?\n>>>")
-port = int(input("Port?\n>>>")) #error will be raised if you enter a string, GUI will solve that
-
-try:
-	s.connect((chat_server,port))
-	print ("Connected to:",chat_server+":"+str(port))
-	data = s.recv(2048) #recieve the message of the day
-	print("Message of the day: " + str(data.decode('utf-8'))) #currently just a random quote
-	s.send(str.encode(username)) #inform the server of your username
-except socket.error as e: #couldn't connect to given IP + port
-	print("Cound not connect to",chat_server+":"+str(port) + "\n" + str(e))
-	os._exit(1)
 
 def send_messages(): #the function that sends messages
 	while True:
@@ -105,6 +50,57 @@ def recieve_messages(): #the function that recieves messages
 			print("Disconnected from:", chat_server)
 			break
 	return
+
+s=socket
+chat_server="127.0.0.1"
+port=5555
+username=""
+developer_mode=0
+
+command_dict={"-help":"\t\t\tview this page",
+			  "-quit":"\t\t\texit program",
+			  "-save prof [name]":"save profile settings as [name]",
+			  "-del prof [name]":"delete profile [name]",
+			  "-view prof":"\t\tviews your saver profiles",
+			  "-edit prof [name]":"edit profile [name]"}
+
+root_command_dict={"terminate":"\t\tterminates the server",
+				   "-kick [username]":"kicks [username] from the server"}
+
+if (input("Load profile or connect to an unsaved server?y/n")).lower() == 'y':
+	print("not implemented yet")
+	# Add code to import a profile  here
+	os._exit(0)  # since no profile is selected, it is useless to try to connect
+else:
+	username = input("Enter your username:\n>>>")
+	if username == "root":  # enables client developer mode
+		developer_mode = True  # Boolean that keeps track of wether developer mode is active or not
+		print("You are now root user (admin)")
+		username = input("Username on server?\n")  # to make it possible to enable client dev mode without connecting as root
+
+while True:
+		address_protocol = input("IP protocol? (IPv4 or IPv6)\n>>>")
+		if address_protocol.lower() == "ipv4":
+			break #IPv4 is a valid protocol
+		elif address_protocol.lower() == "ipv6":
+			s=socket.socket(socket.AF_INET6, socket.SOCK_STREAM) #creates a socket object for IPv6
+			break #IPv6 is a valid protocol
+		else:
+			print("Invalid network protocol")
+
+#comment these away if you want to connect to 127.0.0.1:5555
+chat_server = input("IP address?\n>>>")
+port = int(input("Port?\n>>>")) #error will be raised if you enter a string, GUI will solve that
+
+try:
+	s.connect((chat_server,port))
+	print ("Connected to:",chat_server+":"+str(port))
+	data = s.recv(2048) #recieve the message of the day
+	print("Message of the day: " + str(data.decode('utf-8'))) #currently just a random quote
+	s.send(str.encode(username)) #inform the server of your username
+except socket.error as e: #couldn't connect to given IP + port
+	print("Cound not connect to",chat_server+":"+str(port) + "\n" + str(e))
+	os._exit(1)
 
 sender = threading.Thread(target=send_messages,daemon=0)
 reciever = threading.Thread(target=recieve_messages,daemon=0)
