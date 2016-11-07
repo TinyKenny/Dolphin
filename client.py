7ea64472606c9786ba59e4974b065af1792bd879
albatrossPasta <lukastonneman@hotmail.com>
2016-11-07 19:44:59 +0100

    patch to latest version of client & server

--- client.py
+++ client.py
@@ -1,96 +1,267 @@
 import socket
 import os
 import threading
+import configparser
+
+def spacer(spaces, string):
+	return (spaces - len(string)) * " "
+
+def command_interpreter(client_message):
+	if client_message == "/quit":
+		print("Closing...")
+		return 1
+
+	elif client_message == "/help":
+		for command, desc in command_dict.items():
+			print(command + desc)
+		s.send(str.encode(client_message))
+		print(s.recv(2048).decode("utf-8"))
+		if developer_mode:
+			print("ROOT COMMANDS:")
+			for command, desc in root_command_dict.items():
+				print(command + desc)
+
+	elif client_message[0:6] == "/save ":
+		configEditor = configparser.RawConfigParser()
+
+		for profile in config.sections():#lägga till de gamla profilerna i den nya filen
+			configEditor.add_section(profile)
+			configEditor.set(profile, "ip", config[profile]["ip"])
+			configEditor.set(profile, "port", config[profile]["port"])
+			configEditor.set(profile, "network_protocol", config[profile]["network_protocol"])
+			configEditor.set(profile, "username", config[profile]["username"])
+			if developer_mode:
+				configEditor.set(profile, "developer_mode", "True")
+
+		#lägga till den nya profilen till filen
+		prof_name=client_message[6:]
+		configEditor.add_section(prof_name)
+		configEditor.set(prof_name, "ip", chat_server)
+		configEditor.set(prof_name, "port", str(port))
+		configEditor.set(prof_name, "network_protocol", network_protocol)
+		configEditor.set(prof_name, "username", username)
+		if developer_mode:
+			configEditor.set(prof_name, "developer_mode", "True")
+
+		with open('profiles.ini', 'w') as new_configfile:
+			configEditor.write(new_configfile)
+			config.read("profiles.ini")
+			print("Done!")
+
+	elif client_message[0:5] == "/del ":
+		if not (client_message[5:] in config.sections()):
+			print("No such profile:"+client_message[5:])
+			return 0
+
+		configEditor = configparser.RawConfigParser()
+
+		for profile in config.sections():#lägga till de gamla profilerna i den nya filen
+			if profile==client_message[5:]:
+				continue
+			configEditor.add_section(profile)
+			configEditor.set(profile, "ip", config[profile]["ip"])
+			configEditor.set(profile, "port", config[profile]["port"])
+			configEditor.set(profile, "network_protocol", config[profile]["network_protocol"])
+			configEditor.set(profile, "username", config[profile]["username"])
+			if developer_mode:
+				configEditor.set(profile, "developer_mode", "True")
+
+		with open('profiles.ini', 'w') as new_configfile:
+			configEditor.write(new_configfile)
+			config.read("profiles.ini") #detta fungerar inte, ingen aning om varför, kan inte hitta en lösning
+			print("Done!")
+
+	elif client_message[0:11] == "/edit -all ":
+		if not (client_message[11:] in config.sections()):
+			print("No such profile:"+client_message[16:])
+			return 0
+
+		configEditor = configparser.RawConfigParser()
+
+		for profile in config.sections():  # lägga till profilerna i den nya filen
+			if profile == client_message[11:]: #den som ska ändas har hittats
+				print("Current name:"+profile)
+				new_profile_name = input("Enter new name:\n>>>")
+				configEditor.add_section(new_profile_name)
+				print("Current IP:"+config[profile]["ip"])
+				configEditor.set(new_profile_name, "ip", input("Enter new IP addres:\n>>>"))
+				print("Current port:" + str(config[profile]["port"]))
+				configEditor.set(new_profile_name, "port", input("Enter new port:\n>>>"))
+				print("Current network protocol:" + config[profile]["network_protocol"])
+				configEditor.set(new_profile_name, "network_protocol", input("Enter new network protocol:\n>>>"))
+				print("Current username:" + config[profile]["username"])
+				configEditor.set(new_profile_name, "username", input("Enter new username:\n>>>"))
+				if developer_mode:
+					if not input("Enter new dev mode status(True/False)\n>>>") == "False":
+						configEditor.set(new_profile_name, "developer_mode", True)
+			else: #detta var inte profilen som skulle ändras, lägger till den som den är i den nya filen
+				configEditor.add_section(profile)
+				configEditor.set(profile, "ip", config[profile]["ip"])
+				configEditor.set(profile, "port", config[profile]["port"])
+				configEditor.set(profile, "network_protocol", config[profile]["network_protocol"])
+				configEditor.set(profile, "username", config[profile]["username"])
+				try:
+					if config[profile]["developer_mode"]:
+						configEditor.set(profile, "developer_mode", "True")
+				except KeyError as e:
+					pass #profilen var ej developer, inget ska hända
+
+		with open('profiles.ini', 'w') as new_configfile:
+			configEditor.write(new_configfile)
+			config.read("profiles.ini") #detta fungerar inte, ingen aning om varför, kan inte hitta en lösning
+
+	elif client_message[0:6] == "/edit ":
+		if not (client_message[6:] in config.sections()):
+			print("No such profile:"+client_message[6:])
+			return 0
+
+		configEditor = configparser.RawConfigParser()
+
+		for profile in config.sections():  # lägga till profilerna i den nya filen
+			if profile == client_message[6:]: #den som ska ändas har hittats
+				print("Current name:"+profile)
+				new_profile_name = input("Enter new profile name:\n>>>")
+				configEditor.add_section(new_profile_name)
+				print("Current IP:"+config[profile]["ip"])
+				configEditor.set(new_profile_name, "ip", input("Enter new IP addres:\n>>>"))
+				configEditor.set(new_profile_name, "port", config[profile]["port"]) #sker auto
+				configEditor.set(new_profile_name, "network_protocol", config[profile]["network_protocol"])#sker auto
+				print("Current username:" + config[profile]["username"])
+				configEditor.set(new_profile_name, "username", input("Enter new username:\n>>>"))
+				try:
+					if config[profile]["developer_mode"]:
+						configEditor.set(new_profile_name, "developer_mode", "True")
+				except KeyError as e:
+					pass #profilen var ej developer, inget ska hända
+			else:
+				configEditor.add_section(profile)
+				configEditor.set(profile, "ip", config[profile]["ip"])
+				configEditor.set(profile, "port", config[profile]["port"])
+				configEditor.set(profile, "network_protocol", config[profile]["network_protocol"])
+				configEditor.set(profile, "username", config[profile]["username"])
+				try:
+					if config[profile]["developer_mode"]:
+						configEditor.set(profile, "developer_mode", "True")
+				except KeyError as e:
+					pass #profilen var ej developer, inget ska hända
+
+		with open('profiles.ini', 'w') as new_configfile:
+			configEditor.write(new_configfile)
+			config.read("profiles.ini") #detta fungerar inte, ingen aning om varför, kan inte hitta en lösning
+
+	elif client_message == "/view -all":
+		print("Profile name" + spacer(15, "Profile name") +
+			  "IP addres" + spacer(13, "IP addres") +
+			  "Port" + spacer(8, "port") +
+			  "Network Protocol  " +
+			  "Username")
+		for profile in config.sections():
+			print(profile +
+				  (15 - len(profile)) * " " +
+				  config[profile]["ip"] +
+				  (13 - len(config[profile]["ip"])) * " " +
+				  config[profile]["port"] +
+				  (8 - len(config[profile]["port"])) * " " +
+				  config[profile]["network_protocol"] +
+				  (18 - len(config[profile]["network_protocol"])) * " " +
+				  config[profile]["username"])
+
+	elif client_message == "/view":
+		print("Profile name" + spacer(15, "Profile name") +
+			  "IP addres" + spacer(13, "IP addres") +
+			  "Username")
+		for profile in config.sections():
+			print(profile +
+				  (15 - len(profile)) * " " +
+				  config[profile]["ip"] +
+				  (13 - len(config[profile]["ip"])) * " " +
+				  config[profile]["username"])
+
+	else:
+		s.send(str.encode(client_message)) #kommandot hittades inte utan skickas till servern
+		pass # så att man enkelt ska kunna "döja" hela denna kod del i pycharm
 
 def send_messages(): #the function that sends messages
 	while True:
 		client_message = input(">>>")
-		if client_message[0:1] == "-": #lauches command intepreter
-			if client_message=="-quit":
-				break
-			elif client_message=="-help":
-				for command, desc in command_dict.items():
-					print(command +"\t" +desc)
-				if developer_mode:
-					print("ROOT COMMANDS:")
-					for command, desc in root_command_dict.items():
-						print(command + "\t" + desc)
-
-			elif client_message[0:5]=="-kick":
-				s.send(str.encode(client_message))
-
-			elif client_message[0:11] == "-save prof ":
-				print("command not implemented")
-
-			elif client_message[0:10] == "-del prof ":
-				print("command not implemented")
-
-			elif client_message[0:11] == "-edit prof ":
-				print("command not implemented")
-
-			elif client_message[0:10] == "-view prof":
-				print("command not implemented")
-
-			else:
-				print("Unknown command:"+client_message)
+		if client_message[0:1] == "/":
+			if command_interpreter(client_message): #lauches command intepreter
+				s.close()
+				os._exit(0)
 		else:
-			s.send(str.encode(client_message))
-	s.close()
-	return
+			s.send(str.encode(client_message)) #was not a command
 
 def recieve_messages(): #the function that recieves messages
-	while True:
-		try:
-			message_data = s.recv(2048)
-			server_message = message_data.decode('utf-8')
+	try:
+		while True:
+			server_message = s.recv(2048).decode('utf-8')
+			if not server_message:
+				raise ConnectionError #meddelandet är tomt, linux har lite svårt att fatta när det är dags att gå hem annars
 			print (server_message)
-			if server_message=="root:-kick " + username:
-				raise ConnectionError
-		except ConnectionError as e: #this will happen when the user enters "quit" or when the server is shut down
-			print("Disconnected from:", chat_server)
-			break
-	return
+	except ConnectionError as e: #this will happen when the server is shut down
+		print("Disconnected from:", chat_server)
 
-s=socket
 chat_server="127.0.0.1"
 port=5555
 username=""
 developer_mode=0
+config = configparser.ConfigParser()
+config.read("profiles.ini")
+network_protocol=""
 
-command_dict={"-help":"\t\t\tview this page",
-			  "-quit":"\t\t\texit program",
-			  "-save prof [name]":"save profile settings as [name]",
-			  "-del prof [name]":"delete profile [name]",
-			  "-view prof":"\t\tviews your saver profiles",
-			  "-edit prof [name]":"edit profile [name]"}
+command_dict={"/help":spacer(20, "/help") + "view this page",
+			  "/quit":spacer(20, "/quit") + "exit program",
+			  "/save [name]":spacer(20, "/save [name]") + "save profile settings as [name]",
+			  "/del [name]":spacer(20, "/del [name]") + "delete profile [name]",
+			  "/view":spacer(20, "/view") + "views your saved profiles",
+			  "/edit [name]":spacer(20, "/edit [name]") + "edit profile [name]",
+			  "/edit -all [name]":spacer(20, "/edit -all [name]") + "edit all avalible information about profile [name]",
+			  "/view -all":spacer(20, "/view -all") + "views all inofmation avalible about you profiles"}
 
-root_command_dict={"terminate":"\t\tterminates the server",
-				   "-kick [username]":"kicks [username] from the server"}
+root_command_dict={"/terminate":spacer(20, "/terminate") + "terminates the server",
+				   "/kick [username]":spacer(20, "/kick [username]") + "kicks [username] from the server"}
 
-if (input("Load profile or connect to an unsaved server?y/n")).lower() == 'y':
-	print("not implemented yet")
-	# Add code to import a profile  here
-	os._exit(0)  # since no profile is selected, it is useless to try to connect
-else:
+print("Select a proile to use or select manual:")
+print("* manual")
+for n in config.sections():
+	print("*", n)
+
+profile = input(">>>")
+if profile in config.sections():
+	network_protocol = str(config[profile]["network_protocol"])
+	if network_protocol=="IPv4":
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	elif network_protocol=="IPv6":
+		s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+	else:
+		print("Corrupt profile")
+		os._exit(1)
+
+	try:
+		port = config.getint(profile, "port")
+	except ValueError:
+		print("Corrupt profile")
+		os._exit(1)
+
+	chat_server = config.get(profile, "ip")
+	username = config.get(profile, "username")
+	developer_mode= config.getboolean(profile, "developer_mode", fallback=False)
+
+elif profile == "manual":
 	username = input("Enter your username:\n>>>")
 	if username == "root":  # enables client developer mode
 		developer_mode = True  # Boolean that keeps track of wether developer mode is active or not
 		print("You are now root user (admin)")
 		username = input("Username on server?\n")  # to make it possible to enable client dev mode without connecting as root
-
-while True:
-		address_protocol = input("IP protocol? (IPv4 or IPv6)\n>>>")
-		if address_protocol.lower() == "ipv4":
-			break #IPv4 is a valid protocol
-		elif address_protocol.lower() == "ipv6":
-			s=socket.socket(socket.AF_INET6, socket.SOCK_STREAM) #creates a socket object for IPv6
-			break #IPv6 is a valid protocol
-		else:
-			print("Invalid network protocol")
-
-#comment these away if you want to connect to 127.0.0.1:5555
-chat_server = input("IP address?\n>>>")
-port = int(input("Port?\n>>>")) #error will be raised if you enter a string, GUI will solve that
+	network_protocol = input("Enter IP protocol(IPv4 or IPv6):\n>>>")
+	if network_protocol.lower() == "ipv4":
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		network_protocol = "IPv4"
+	elif network_protocol.lower() == "ipv6":
+		s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+		network_protocol = "IPv6"
+	chat_server=input("Enter server IP:\n>>>")
+	port = int(input("Enter port:\n>>>"))
 
 try:
 	s.connect((chat_server,port))
@@ -102,9 +273,7 @@ except socket.error as e: #couldn't connect to given IP + port
 	print("Cound not connect to",chat_server+":"+str(port) + "\n" + str(e))
 	os._exit(1)
 
-sender = threading.Thread(target=send_messages,daemon=0)
-reciever = threading.Thread(target=recieve_messages,daemon=0)
+sender = threading.Thread(target=send_messages, daemon=0)
+reciever = threading.Thread(target=recieve_messages, daemon=0)
 sender.start()
 reciever.start()
-sender.join()
-reciever.join()
--- server.py
+++ server.py
@@ -29,63 +29,131 @@ def getRaddr(conn):
     return raddr
 
 def listenToClient(conn, username):
-    raddr=""
-    try:
-        raddr=getRaddr(conn)
-    except:
-        raddr = getRaddr(conn)
-    while 1:
-        try:
-            cmh.common_message = username + ":" + (conn.recv(2048)).decode("utf-8")
-            print(cmh.common_message)
-            thread_manager.acquire() #hämtar managern
-            thread_manager.notify()  #notifera en random tråd som vändtar, kräver att managern är i tråden
-            thread_manager.release()  # detta gör att manangern kan gå till andra trådar
-            if cmh.common_message=="root:terminate":
-                print("Terminating server")
-                s.close()
-                os._exit(0)
-        except ConnectionResetError:
-            cmh.common_message= str(username) + " disconnected"
-            break
+	global taken_usernames, help, root_help
+	raddr=""
+	try:
+		raddr=getRaddr(conn)
+	except:
+		raddr = getRaddr(conn)
+	cmh.common_message = username + " connected"
+	thread_manager.acquire()
+	thread_manager.notify_all()
+	thread_manager.release()
+	while True:
+		try:
+			cmh.common_message = username + ":" + (conn.recv(2048)).decode("utf-8")
+			print(cmh.common_message)
+			if cmh.common_message.startswith(username+":/"): #commands
+				if username=="root":
+					if cmh.common_message=="root:/terminate":
+						print("Terminating server")
+						s.close()
+						os._exit(0)
+					elif cmh.common_message=="root:/users":
+						cmh.common_message=cmh.common_message+"\nCurrently connected users:"
+						for u in taken_usernames:
+							cmh.common_message=cmh.common_message+"\n"+u
+					elif cmh.common_message=="root:/enumerate":
+						conn.send(str.encode("Number of live threads: "+str(threading.active_count())))
+						for t in threading.enumerate():
+							conn.send(str.encode(str(t)+"\n"))
+						cmh.common_message=""
+					elif cmh.common_message.startswith("root:/kick "):
+#						if cmh.common_message[11:] in taken_usernames:
+#							if taken_usernames[cmh.common_message[11:]]:
+#								taken_usernames[cmh.common_message[11:]]=False
+#							elif not taken_usernames[cmh.common_message[11:]]:
+#								conn.send(str.encode("That user is not connected"))
+						if cmh.common_message[11:] not in taken_usernames:
+							conn.send(str.encode("That user is not connected"))
+				if cmh.common_message.startswith(username+":/help"):
+					if username == "root":
+						conn.send(str.encode(help+root_help))
+					else:
+						conn.send(str.encode(help))
+					cmh.common_message=""
+				elif cmh.common_message.startswith(username+":/me"):
+					cmh.common_message=username+cmh.common_message[len(username+":/me"):]
+			thread_manager.acquire() #hämtar managern
+			thread_manager.notify_all()  #notifera en random tråd som vändtar, kräver att managern är i tråden
+			thread_manager.release()  # detta gör att manangern kan gå till andra trådar
+		except ConnectionResetError:
+			cmh.common_message= str(username) + " disconnected"
+			thread_manager.acquire()
+			thread_manager.notify_all()
+			thread_manager.release()
+			break
+		except ConnectionAbortedError:
+			cmh.common_message= str(username) + " was kicked"
+			thread_manager.acquire()
+			thread_manager.notify_all()
+			thread_manager.release()
+			break
 
-def sendToClient(conn):
-    while 1:
-        thread_manager.acquire() #hämtar managern
-        thread_manager.wait() #säger till managern att "jag väntar på att någon ska notifiera mig"
-                              #automatiskt: thread_manager.release() #se rad 3 under
-                              # när den har blivt notifierad så hämtar den managern
-        thread_manager.notify() #notifera en random tråd som vändtar, kräver att managern är i tråden
-                                # detta sker även här för att notify ska sprida sig till alla
-        thread_manager.release() #detta gör att manangern kan gå till andra trådar
-        try:
-            conn.sendall(str.encode(cmh.common_message))
-            time.sleep(0.01) #för att hindra den från att notifiera sig själv
-        except ConnectionResetError:
-            pass
+def sendToClient(conn, listener, username):
+	global taken_usernames
+	while listener.is_alive():
+		thread_manager.acquire() #hämtar managern
+		thread_manager.wait() #säger till managern att "jag väntar på att någon ska notifiera mig"
+							  #automatiskt: thread_manager.release() #se rad 3 under
+							  # när den har blivt notifierad så hämtar den managern
+#        thread_manager.notify() #notifera en random tråd som vändtar, kräver att managern är i tråden
+							  # detta sker även här för att notify ska sprida sig till alla
+		thread_manager.release() #detta gör att manangern kan gå till andra trådar
+		if cmh.common_message == ("root:/kick "+username):
+			conn.send(str.encode("You were kicked out <3"))
+#			taken_usernames[username]=False
+			taken_usernames.remove(username)
+			conn.close()
+			break
+		else:
+			try:
+				conn.sendall(str.encode(cmh.common_message))
+				time.sleep(0.01) #för att hindra den från att notifiera sig själv
+			except ConnectionResetError:
+				pass
 
 def clientHandler(sock):
-    random_welcome_message = ["You want the crucible? I am the crucible.",
+	global taken_usernames
+	random_welcome_message = ["You want the crucible? I am the crucible.",
                               "FIGHT ON GERUDIAN!!!", "I can't believe what I'm seeing!",
                               "You can fight by my side anytime, Gaurdian",
                               "Is english class canceld tomorrow?"]
-    sock.send(str.encode(random_welcome_message[random.randint(0, (len(random_welcome_message) - 1))]))
-    username = (sock.recv(2048)).decode("utf-8")
-    raddr = getRaddr(sock)
-    sender = threading.Thread(target=sendToClient,
-                              daemon=1,
-                              kwargs={'conn':sock},
-                              name="S" + raddr)
-    listner= threading.Thread(target=listenToClient,
+	sock.send(str.encode(random_welcome_message[random.randint(0, (len(random_welcome_message) - 1))]))
+	username = (sock.recv(2048)).decode("utf-8")
+	raddr = getRaddr(sock)
+	
+	if username in taken_usernames:
+		sock.send(str.encode("That username is already taken."))
+		print("Disconnected from "+raddr)
+		sock.close()
+		return
+	elif username not in taken_usernames:
+		taken_usernames.update([username])
+	else:
+		print("shit got weird")
+		sock.send(str.encode("Something is wrong. Please report this event, and what you did to make this happen, to the server developer"))
+		print("Disconnected from " + raddr)
+		sock.close()
+		return
+
+	listener= threading.Thread(target=listenToClient,
                               daemon=1,
                               kwargs={'conn':sock, 'username':username},
-                              name="L" + raddr)
-    sender.start()
-    listner.start()
+                              name="L-" + username)
+	sender = threading.Thread(target=sendToClient,
+							  daemon=1,
+                              kwargs={'conn':sock, 'listener':listener, 'username':username},
+                              name="S-" + username)
+	listener.start()
+	sender.start()
+	
+	while listener.is_alive():
+		time.sleep(0.1)
+	if username in taken_usernames:
+		taken_usernames.remove(username)
+	print("Disconnected from " + raddr)
 
-    while listner.is_alive():
-        time.sleep(0.1)
-    print("Disconnected from " + raddr)
 if platform == "win32":
 	os.system("mode con: cols=90 lines=30")
 config = configparser.ConfigParser()
@@ -99,7 +167,7 @@ if str.lower(profile) == "new":
 		profile = input("Profile name: ")
 	config[profile] = {}
 	network_protocol = input("Network protocol (IPv4/IPv6): ")
-	while str.lower(network_protocol) != ("ipv4" and "ipv6"):
+	while (str.lower(network_protocol) != "ipv4") and (str.lower(network_protocol) != "ipv6"):
 		print("Invalid protocol, it has to be IPv4 or IPv6!")
 		network_protocol = str(input("Network protocol (IPv4/IPv6): "))
 	config[profile]["network_protocol"] = network_protocol
@@ -126,17 +194,30 @@ else:
 	network_protocol= str(config[profile]["network_protocol"])
 	port = int(config[profile]["port"])
 	max_population= int(config[profile]["max_population"])
-host = ''
+host = '0.0.0.0'
 serverIP="placeholder4serverIP"
 client_handlers=[]
 cmh = CommmonMessageHoster()
 lock = threading.Lock()
 thread_manager = threading.Condition(lock) #tänk att detta är en manager som trådarna måste ha närvanade när det gör saker
 s = socket
+taken_usernames=set()
+help=""
+root_help="ROOT COMMANDS:"
+command_dict={"/help":"View this message.",
+			  "/me":"Works like the irc command"}
+root_command_dict={"/users":"Returns a list of all connected users",
+				   "/kick [username]":"Kicks the specified user.",
+				   "/terminate":"Terminates the server",
+				   "/enumerate":"Returns a list of all live threads"}
 
+for command in command_dict:
+	help+=command+" "*(20-len(command))+command_dict[command]+"\n"
+for command in root_command_dict:
+	root_help+="\n"+command+" "*(20-len(command))+root_command_dict[command]
 
 if str.lower(network_protocol)=="ipv6":
-    s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+	s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 elif str.lower(network_protocol)=="ipv4":
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 else:
@@ -155,19 +236,18 @@ print("Max population:",max_population)
 print("Listening @ ",serverIP + ":" + str(port))
 
 while 1:
-    for n in range(len(client_handlers)):
-        if not client_handlers[n].is_alive():
-            client_handlers.pop(n)
-            break
+	for n in range(len(client_handlers)):
+		if not client_handlers[n].is_alive():
+			client_handlers.pop(n)
+			break
 
-    sock, address = s.accept()
-    if len(client_handlers) > max_population:
-        sock.send(str.encode("Server full, try again later"))
-        sock.close()
-        continue #skippar resten av loopen och börjar om från början
-
-    client_handlers.append(threading.Thread(kwargs={'sock':sock}, #passar sock som argument. kwargs=keyword arguments
+	sock, address = s.accept()
+	if len(client_handlers) > max_population:
+		sock.send(str.encode("Server full, try again later"))
+		sock.close()
+		continue #skippar resten av loopen och börjar om från början
+	print('connected to ' + address[0] + ':' + str(address[1]))
+	client_handlers.append(threading.Thread(kwargs={'sock':sock}, #passar sock som argument. kwargs=keyword arguments
                                             target=clientHandler, # när den startar kommer den att starta med clientHandler
                                             daemon=1))            # när programmet avslutas så dör även threaden (kanske löser linux upptagna portar?)
-    client_handlers[len(client_handlers)-1].start()               # startar threaden
-    print('connected to ' + address[0] + ':' + str(address[1]))
+	client_handlers[len(client_handlers)-1].start()               # startar threaden
