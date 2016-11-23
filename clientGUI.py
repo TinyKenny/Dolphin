b3dc34d603e4f15478e2208ba1ca368edb246667
albatrossPasta <lukastonneman@hotmail.com>
2016-11-23 13:31:10 +0100

    see disc
    can connect to server,
    more work on info frame
    can connect & disconnect without closing window

--- clientGUI.py
+++ clientGUI.py
@@ -6,19 +6,21 @@ import threading
 import configparser
 import time
 
+version="Version 0.0.0.2"
+
 class GUI:
     def spacer(self, spaces, string):
         return (spaces - len(string)) * " "
 
     def command_interpreter(self, client_message):
         if client_message == "/quit":
-            self.print_to_log("Closing...")
-            return 1
+            self.print_to_log("Disconnecting...")
+            self.s.close()
 
         elif client_message == "/help":
             for command, desc in self.command_dict.items():
                 self.print_to_log(command + desc)
-            s.send(str.encode(client_message))
+            self.s.send(str.encode(client_message))
 
         elif client_message[0:6] == "/save ":
             configEditor = configparser.RawConfigParser()
@@ -178,18 +180,17 @@ class GUI:
                       config[profile]["username"])
 
         else:
-            #s.send(str.encode(client_message))
-            self.print_to_log(self.user_input.get())
+            self.s.send(str.encode(client_message))
 
     def recieve_messages(self):  # the function that recieves messages
         try:
             while True:
-                server_message = s.recv(2048).decode('utf-8')
+                server_message = self.s.recv(2048).decode('utf-8')
                 if not server_message:
                     raise ConnectionError  # meddelandet är tomt, linux har lite svårt att fatta när det är dags att gå hem annars
-                gui_obj.print_to_log(gui_obj, server_message)
+                self.print_to_log(server_message)
         except ConnectionError as e:  # this will happen when the server is shut down
-            gui_obj.print_to_log(gui_obj, ("Disconnected from:" + chat_server))
+            self.print_to_log("Disconnected from:" + chat_server.get())
 
     def print_to_log(self, msg):
         self.msg_log.insert(END, msg)
@@ -199,75 +200,98 @@ class GUI:
     def read_input(self):  #does not work work woth server
         if self.user_input.get() == '':
             return 0
-        if self.user_input.get()[0:1] == "/":
-            if self.command_interpreter(self.user_input.get()):  # lauches command intepreter
-                s.close()
-                os._exit(0)
-        else:
-            #s.send(str.encode(self.user_input.get()))  # was not a command
-            self.print_to_log((self.username +  ":" + self.user_input.get()))
-        self.user_input.set("")
+        try:
+            if self.user_input.get()[0:1] == "/":
+                self.command_interpreter(self.user_input.get())  # lauches command intepreter
+            else:
+                self.s.send(str.encode(self.user_input.get()))  # was not a comman
+
+        except OSError:#not connectde to a sever
+            self.print_to_log(self.user_input.get())
+        except AttributeError:
+            self.print_to_log(self.user_input.get())#same
+        finally:
+            self.user_input.set("")
 
     def manual_connect(self):
         connet_window=Tk()
         connet_window.title("Manual Connect")
         #username
         Label(connet_window, text="Username").grid(row=0, column=0, sticky='E')
-        username.set('bob')
         username_field=Entry(connet_window,
                          width=16,
                          exportselection=0,
-                         textvariable=username,
+                         textvariable= self.username,
                          bg="white")
         username_field.grid(row=0, column=1, sticky='W', pady=2)
 
         #ip
         Label(connet_window, text="IP adresss").grid(row=1, column=0, sticky='E')
-
-        chat_server.set("127.0.0.1")
         chat_server_field=Entry(connet_window,
                               width=16,
                               exportselection=0,
-                              textvariable=chat_server,
+                              textvariable=self.chat_server,
                               bg="white")
         chat_server_field.grid(row=1, column=1, pady=2)
 
         #port
         Label(connet_window, text="Port").grid(row=2, column=0, sticky='E')
-        port=IntVar()
-        port.set(5555)
         port_field=Entry(connet_window,
                          width=5,
                          exportselection=0,
-                         textvariable=port,
+                         textvariable=self.port,
                          bg="white")
         port_field.grid(row=2, column=1, sticky='W', pady=2)
 
         #network protocol
+        def setIPv4():
+            self.network_protocol.set("IPv4")
+
+        def setIPv6():
+            self.network_protocol.set("IPv6")
         network_protocol_frame=LabelFrame(connet_window, text="Network Protocol")
         network_protocol_frame.grid(row=3, columnspan=2, pady=2)
-
-        ipv4_butt = Radiobutton(network_protocol_frame, text="IPv4", variable=network_protocol, value="IPv4")
+        ipv4_butt = Radiobutton(network_protocol_frame,
+                                text="IPv4", variable=self.network_protocol,
+                                value='IPv4',
+                                command=setIPv4)
         ipv4_butt.grid()
         ipv4_butt.select()
-
-        ipv6_butt = Radiobutton(network_protocol_frame, text="IPv6", variable=network_protocol, value="IPv6")
+        ipv6_butt = Radiobutton(network_protocol_frame,
+                                text="IPv6",
+                                variable=self.network_protocol,
+                                value='IPv6',
+                                command=setIPv6)
         ipv6_butt.grid()
 
         #buttons frame
         def cancel():
-            username.set('')
-            chat_server.set('')
-            port.set('')
+            self.username.set('')
+            self.chat_server.set('')
+            self.port.set('')
+            self.network_protocol.set('')
             connet_window.destroy()
 
+        def connect():
+            '''print(self.username.get())
+            print(self.network_protocol.get())
+            print(self.chat_server.get())
+            print(self.port.get())'''
+            if self.chat_server.get()=="":
+                self.username.set("bob")
+                self.port.set(5555)
+                self.chat_server.set("127.0.0.1")
+                ipv4_butt.invoke() #sätter IPv4
+            self.connect()
+            connet_window.destroy()
 
         butt_frame=Frame(connet_window)
         butt_frame.grid(row=4, columnspan=2, pady=4)
         connect_butt=Button(butt_frame,
                             text="Connect",
                             fg='green',
-                            activeforeground='green')
+                            activeforeground='green',
+                            command=connect)
         cancel_butt=Button(butt_frame,
                            text="Cancel",
                            fg='red',
@@ -279,47 +303,32 @@ class GUI:
         connet_window.mainloop()
 
     def disconnect(self):
-        print("Disconnecting")
+        print("Disconnecting...")
+        self.s.close()
         self.c_or_dc.set("Connect")
         pass
 
     def connect(self):
-        if network_protocol == "IPv4":
+        if self.network_protocol.get() == "IPv4":
             self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        elif network_protocol == "IPv6":
+        elif self.network_protocol.get() == "IPv6":
             self.s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
         else:
-            print("Corrupt profile")
+            self.print_to_log("Corrupt profile or invalid input")
         try:
-            self.s.connect((chat_server, port))
-            self.print_to_log(("Connected to:", chat_server + ":" + str(port)))
-            data = s.recv(2048)  # recieve the message of the day
+            self.s.connect((self.chat_server.get(), self.port.get()))
+            self.print_to_log(("Connected to: " + self.chat_server.get() + ":" + str(self.port.get())))
+            data = self.s.recv(2048)  # recieve the message of the day
             self.print_to_log("Message of the day: " + str(data.decode('utf-8')))  # currently just a random quote
-            s.send(str.encode(username))  # inform the server of your username
+            self.s.send(str.encode(self.username.get()))  # inform the server of your username
         except socket.error as e:  # couldn't connect to given IP + port
-            self.print_to_log(("Cound not connect to", chat_server + ":" + str(port) + "\n" + str(e)))
+            self.print_to_log(("Cound not connect to", self.chat_server.get() + ":" + str(self.port.get()) + "\n" + str(e)))
 
         reciever = threading.Thread(target=self.recieve_messages, daemon=0)
         reciever.start()
+        self.c_or_dc.set("Disconnect")
 
-    def __init__(self, window):
-
-        self.chat_server = StringVar()
-        self.port = IntVar()
-        self.username = StringVar
-        self.network_protocol = StringVar()
-        self.config = configparser.ConfigParser()
-        self.config.read("profiles.ini")
-
-        self.command_dict = {"/help": self.spacer(20, "/help") + "view this page",
-                        "/quit": self.spacer(20, "/quit") + "exit program",
-                        "/save [name]": self.spacer(20, "/save [name]") + "save profile settings as [name]",
-                        "/del [name]": self.spacer(20, "/del [name]") + "delete profile [name]",
-                        "/view": self.spacer(20, "/view") + "views your saved profiles",
-                        "/edit [name]": self.spacer(20, "/edit [name]") + "edit profile [name]",
-                        "/edit -all [name]": self.spacer(20, "/edit -all [name]") + "edit all avalible information about profile [name]",
-                        "/view -all": self.spacer(20, "/view -all") + "views all inofmation avalible about you profiles"}
-
+    def build_window(self, window):
         window.title("Dolphin")
         self.toolbar_frame = Frame(window, bg="grey")  # the toolbar frame
         self.toolbar_frame.pack(side=TOP, fill=X)
@@ -381,29 +390,32 @@ class GUI:
         self.entry.bind('<Return>', entry_event_handler)  # makes enter key send message
         self.entry.pack(padx=5, pady=5, side=LEFT)
 
-        self.info_keys_color = "#595959"
-        self.info_keys = [Label(self.info_frame, text="Server name:", fg=self.info_keys_color),
-                          Label(self.info_frame, text="IP:", fg=self.info_keys_color)]
+        info_keys_color = "black"
+        self.info_keys = [Label(self.info_frame, text="Server name:", fg=info_keys_color),
+                          Label(self.info_frame, text="IP:", fg=info_keys_color),
+                          Label(self.info_frame, text="Username:", fg=info_keys_color),
+                          Label(self.info_frame, text="Level:", fg=info_keys_color),]
 
-        self.info_values = [Label(self.info_frame, text="Pewds h8 club", fg=self.info_keys_color),
-                            Label(self.info_frame, text="127.0.0.1", fg=self.info_keys_color)]
+        self.info_values = [Label(self.info_frame, text="placeholder", fg=info_keys_color),
+                            Label(self.info_frame, textvariable=self.chat_server, fg=info_keys_color),
+                            Label(self.info_frame, textvariable=self.username, fg=info_keys_color),
+                            Label(self.info_frame, text="placeholder", fg=info_keys_color)]
+
+        for n in range(len(self.info_keys)):
+            self.info_keys[n].grid(row=n, column=0, sticky=E)
+            self.info_values[n].grid(row=n, column=1, sticky=W)
 
         self.ad = Label(self.ad_frame, text="csgogambling.com", relief=GROOVE, fg="yellow", bg="red", bd=1, height=5)
         self.ad.pack(padx=5, pady=5)
 
-        for n in range(len(self.info_keys)):
-            self.info_values[n].grid(row=n, column=0, sticky=E)
-            self.info_values[n].grid(row=n, column=1, sticky=W)
-
         # TOOLBAR BUTTONS
         def conn_or_disconn():
-            print("callss")
-            if self.c_or_dc.get()=="Connect":
+            if self.c_or_dc.get() == "Connect":
                 self.manual_connect()
-            elif self.c_or_dc.get()=="Disconnect":
+            elif self.c_or_dc.get() == "Disconnect":
                 self.disconnect()
 
-        self.c_or_dc=StringVar()
+        self.c_or_dc = StringVar()
         self.c_or_dc.set("Connect")
         connect_butt = Button(self.toolbar_frame,
                               textvariable=self.c_or_dc,
@@ -411,9 +423,29 @@ class GUI:
         profile_butt = Button(self.toolbar_frame, text="Profiles")
         settings_butt = Button(self.toolbar_frame, text="Settings")
 
+        connect_butt.pack(side=LEFT, padx=3, pady=2)
         profile_butt.pack(side=LEFT, padx=3, pady=2)
         settings_butt.pack(side=LEFT, padx=3, pady=2)
-        connect_butt.pack(side=LEFT, padx=3, pady=2)
+
+    def __init__(self, window):
+
+        self.chat_server = StringVar()
+        self.port = IntVar()
+        self.username = StringVar()
+        self.network_protocol = StringVar()
+        self.config = configparser.ConfigParser()
+        self.config.read("profiles.ini")
+
+        self.command_dict = {"/help": self.spacer(20, "/help") + "view this page",
+                        "/quit": self.spacer(20, "/quit") + "exit program",
+                        "/save [name]": self.spacer(20, "/save [name]") + "save profile settings as [name]",
+                        "/del [name]": self.spacer(20, "/del [name]") + "delete profile [name]",
+                        "/view": self.spacer(20, "/view") + "views your saved profiles",
+                        "/edit [name]": self.spacer(20, "/edit [name]") + "edit profile [name]",
+                        "/edit -all [name]": self.spacer(20, "/edit -all [name]") + "edit all avalible information about profile [name]",
+                        "/view -all": self.spacer(20, "/view -all") + "views all inofmation avalible about you profiles"}
+
+        self.build_window(window)
 
 global gui_obj
 def start_gui():
@@ -434,14 +466,13 @@ gui_thread.start()
 
 while 1: #annars hinner inte föstret skapas innan nästa rad och då blir det error
     try:
-        gui_obj.print_to_log("Version 0.0.0.1")
+        gui_obj.print_to_log(version)
         break
     except NameError:
         pass
 
 chat_server=""
 port=5555
-username=""
 developer_mode=0
 config = configparser.ConfigParser()
 config.read("profiles.ini")
